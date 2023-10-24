b1b9ab5c9d3cade291f7b2c85b644668c1e56290
Pablo Moyano <pabmoy1725@gmail.com>
2023-10-24 17:01:05 -0300

    agregamos el ejercicio numero 6

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -3,11 +3,26 @@ codigo a ejecutar si se cumple la condicion logica
 }
  */
 
-// 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+// Ejercicio 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
 
-let numero1 = parseInt (prompt ("ingrese un primer numero"));
+/*  let numero1 = parseInt (prompt ("ingrese un primer numero"));
 let numero2 = parseInt (prompt ("ingrese un segundo numero"));
 
 if(numero1 > numero2) {
     console.log ("La variable numero 1 es mayor a la variable 2");
-}
+}*/
+
+// Ejercicio 6.- 
+
+const numero1 = parseInt (prompt ("ingrese un primer numero"));
+const numero2 = parseInt (prompt ("ingrese un segundo numero"));
+
+if (numero1 > numero2) {
+    console.log ("El numero " +numero1+" es mayor que el numero "+numero2);
+    } else {
+        if (numero2 > numero1) {
+        console.log ("El numero " +numero2+" es mayor que el numero "+numero1);
+    } else {
+        console.log ("Los numeros ingresados son iguales");
+    }
+    }
