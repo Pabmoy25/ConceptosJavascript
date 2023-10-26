d2c5aa484594600a3d60e06c64d7ee2a5c7dbf48
Pablo Moyano <pabmoy1725@gmail.com>
2023-10-26 17:27:59 -0300

    Agregamos el ejercicio 11 de tp1

--- js/ejercicios/tp1-ejercicio11.js
+++ js/ejercicios/tp1-ejercicio11.js
@@ -1,1 +1,22 @@
+// 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
 
+// 2,3,5,7
+
+const numero = parseInt (prompt("Ingrese un número"));
+
+if (numero % 2 !== 0 && numero % 3 !== 0 && numero % 5 !== 0 && numero % 7 !== 0){
+    document.write ("Es número ingresado no es divisible por 2, ni por 3, ni por 5, ni por 7")
+}
+
+if (numero % 2 === 0){
+    document.write ("Es divisible por 2. ")
+}
+if (numero % 3 === 0){
+    document.write ("Es divisible por 3. ")
+}
+if (numero % 5 === 0){
+    document.write ("Es divisible por 5. ")
+}
+if (numero % 7 === 0){
+    document.write ("Es divisible por 7. ")
+}
