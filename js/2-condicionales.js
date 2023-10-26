8b8055f58675afe8d84b11a6ddb7a4e5c2fd2668
Pablo Moyano <pabmoy1725@gmail.com>
2023-10-26 12:45:47 -0300

    agregamos else if al ejercicio 6

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -18,7 +18,7 @@ const numero2 = parseInt(prompt("ingrese un segundo numero"));
 if (numero1 > numero2) {
   document.write("El número " + numero1 + " es mayor que el número " + numero2);
 } else if (numero2 > numero1) {
-  document.write("El número " + numero2 + " es mayor que el número " + numero1);
+  document.write(`El número ${numero2} es mayor que el número ${numero1}`);
 } else {
   document.write("Los números ingresados son iguales ("+numero2+")");
 }
